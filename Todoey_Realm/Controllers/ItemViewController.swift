353c4c1997d5aafc3009a4646ab42fc816fa8819
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-25 16:24:25 +0900

    Add UITableViewDelegate and UITableViewDataSource for ItemViewController

--- Todoey_Realm/Controllers/ItemViewController.swift
+++ Todoey_Realm/Controllers/ItemViewController.swift
@@ -6,8 +6,11 @@
 //
 
 import UIKit
+import RealmSwift
 
 class ItemViewController: UITableViewController {
+    var items: Results<Item>?
+    let realm: Realm = try! Realm()
     var parentCategory: Category? {
         didSet {
             self.initialize(with: self.parentCategory!.name)
@@ -23,5 +26,41 @@ class ItemViewController: UITableViewController {
 extension ItemViewController {
     func initialize(with title: String) {
         self.title = title
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension ItemViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items?.count ?? 1
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        if let item: Item = self.items?[indexPath.row] {
+            cell.textLabel?.text = item.name
+        }
+        else {
+            cell.textLabel?.text = "No items added yet."
+        }
+        return cell
+    }
+}
+
+// MARK: - Create
+
+extension ItemViewController {
+    
+}
+
+// MARK: - Read
+
+extension ItemViewController {
+    func loadItems() {
+        let predicate: NSPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", self.parentCategory!.name)
+        self.items = self.realm.objects(Item.self).filter(predicate)
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
     }
 }
