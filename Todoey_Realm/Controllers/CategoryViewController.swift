22dd8eb78e9ba15d7b61c60132f3bfb821afad4b
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-06-25 15:38:35 +0900

    Add CategoryView DataSource Delegate

--- Todoey_Realm/Controllers/CategoryViewController.swift
+++ Todoey_Realm/Controllers/CategoryViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 class CategoryViewController: UITableViewController {
+    var categories: [Category] = [Category]()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.initialize()
@@ -25,5 +26,13 @@ extension CategoryViewController {
 // MARK: - UITableViewDataSource
 
 extension CategoryViewController {
-    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.categories.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let category: Category = self.categories[indexPath.row]
+        let cell: UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "Cell") as! UITableViewCell
+        cell.textLabel?.text = category.name
+        return cell
+    }
 }
